68e7d89a6392ec3b2257b973231f763b5930b056
jbprestor <jamesbryanprestor@gmail.com>
2020-04-30 20:33:18 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import CreateTodo from "./components/create-todo";
+import EditTodo from "./components/edit-todo";
+import TodosList from "./components/todos-list";
+import DeleteTodo from "./components/delete-todo";
+import logo from "./logo.svg"
+
+
+function App() {
+  return (
+    <Router>
+
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#" target="_blank">
+            <img src={logo} width="80" height="80" alt="LogoPro"></img>
+          </a>
+          <Link to="/" className="navbar-brand">List of To Do</Link>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Todo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={TodosList}></Route>
+        <Route path="/edit/:id" exact component={EditTodo}></Route>
+        <Route path="/create" exact component={CreateTodo}></Route>
+   
+      </div>
+    </Router>
+
+  );
+}
+
+
+
+export default App;
---
+++ src/components/todos-list.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+const Todo = props => (
+    <tr>
+        <td className={props.todo.todo_completed ? 'completed' : ''} >{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+        <td>
+            <button className="buttonDesign" style={{width:80, marginRight:20, borderRadius:5}} >
+                <Link to={"/edit/" + props.todo._id}>Edit</Link>
+            </button>
+            <button className="buttonDesign" style={{width:80 , borderRadius:5}}>
+                <Link to={"/delete/" + props.todo._id}>Remove</Link>
+            </button>
+
+
+        </td>
+    </tr>
+)
+
+export default class TodosList extends Component {
+    _isMounted = false;
+    constructor(props) {
+        super(props);
+        this.state = { todos: [] };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({ todos: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    componentDidUpdate() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({ todos: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    TodosList() {
+        return this.state.todos.map(function (currentTodo, i) {
+            return <Todo todo={currentTodo} key={i}></Todo>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Todo List</h3>
+                <table className="table table-striped table-dark" style={{ marginTop: 20 }}>
+                    <thead >
+                        <tr>
+                            <th>Description</th>
+                            <th>Author</th>
+                            <th>Priority</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.TodosList()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
